e4b87693294a9653d005b905ce7d1401d284d038
Tomoyuki Shimizu <smz2develop@gmail.com>
2022-10-28 17:52:26 +0900

    Make endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simics-ja/go-api-sample
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/simics-ja/go-api-sample/handlers"
+)
+
+func main() {
+
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+  http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+  http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+  http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
