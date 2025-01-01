dfc8acf62fdc65abd36548f65ba2f62fda86d194
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-01 19:12:18 +0900

    Chapter 3 Read a single row

--- main.go
+++ main.go
@@ -35,32 +35,25 @@ func main() {
 		where article_id = ?;
 	`
 
-	rows, err := db.Query(sqlStr, articleID)
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
 
-	defer rows.Close()
-
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		var createdTime sql.NullTime
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
-		}
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		} else {
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
-	fmt.Printf("%+v\n", articleArray)
+
+	fmt.Printf("%+v\n", article)
 
 	// r := mux.NewRouter()
 
