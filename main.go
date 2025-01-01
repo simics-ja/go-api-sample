0da20f074b11807d8c8e3b933dfc6c472c22ab5f
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-01 18:30:38 +0900

    Chapter 3 insert data

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
-import (
-	"log"
-	"net/http"
+// import (
+// 	"log"
+// 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/simics-ja/go-api-sample/handlers"
+// 	"github.com/gorilla/mux"
+// 	"github.com/simics-ja/go-api-sample/handlers"
+// )
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := mux.NewRouter()
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-  r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-  r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
 
-	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
+	// r := mux.NewRouter()
+
+	// r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	// r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	// r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+  // r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+  // r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+  // r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+	// log.Println("server start at port 8080")
+	// log.Fatal(http.ListenAndServe(":8080", r))
 }
