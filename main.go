8f0f72d5fd34745ffb0d533d936f749939699359
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-02 13:44:06 +0900

    Chapter 3 use transaction

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/simics-ja/go-api-sample/models"
 )
 
 func main() {
@@ -28,26 +27,65 @@ func main() {
 	}
 	defer db.Close()
 
-	// * Insert a row
-	article := models.Article{
-		Title: "insert test",
-		Contents: "Can I insert data correctly?",
-		UserName: "saki",
-	}
-
-	const sqlStr =`
-		insert into articles (title, contents, username, nice, created_at) values (?, ?, ?, 0, now());
-	`
-
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
-
+	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+	article_id := 1
+
+	const sqlGetNice = `
+	select nice
+	from articles
+	where article_id = ?;
+	`
+
+	row := tx.QueryRow(sqlGetNice, article_id)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?;`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, article_id)
+	if err != nil {
+		fmt.Println(err)
+		tx.Rollback()
+		return
+	}
+
+	tx.Commit()
+
+	// * Insert a row
+	// article := models.Article{
+	// 	Title: "insert test",
+	// 	Contents: "Can I insert data correctly?",
+	// 	UserName: "saki",
+	// }
+
+	// const sqlStr =`
+	// 	insert into articles (title, contents, username, nice, created_at) values (?, ?, ?, 0, now());
+	// `
+
+	// result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(result.LastInsertId())
+	// fmt.Println(result.RowsAffected())
 
 	// * Read a row
 	// articleID := 1
