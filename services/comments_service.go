706b76d3a47460b091956e75109b70c9741ca08e
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-07 15:55:23 +0900

    Chapter 5 Create services

---
+++ services/comments_service.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"github.com/simics-ja/go-api-sample/models"
+	"github.com/simics-ja/go-api-sample/repositories"
+)
+
+// PostCommentHandlerで使用することを想定したサービス
+// 引数の情報をもとに新しいコメントを作り、結果を返却
+func PostCommentService(comment models.Comment) (models.Comment, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Comment{}, err
+	}
+	defer db.Close()
+
+	newComment, err := repositories.InsertComment(db, comment)
+	if err != nil {
+		return models.Comment{}, err
+	}
+	return newComment, nil
+}
