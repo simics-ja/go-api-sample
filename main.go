27827a249b370118d12e953d79192ea9b8d96499
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-09 17:55:10 +0900

    Chapter 6 create routers

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	"github.com/simics-ja/go-api-sample/controllers"
+	"github.com/simics-ja/go-api-sample/routers"
 	"github.com/simics-ja/go-api-sample/services"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -30,14 +30,8 @@ func main() {
 
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
-	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
