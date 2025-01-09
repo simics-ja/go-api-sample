1c7ced978fa8313db2d92c8e9ce5ac4b893fc81d
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-09 18:15:46 +0900

    Chapter 6 interface-based abstraction

---
+++ controllers/services/services.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"github.com/simics-ja/go-api-sample/models"
+)
+
+type MyAppServicer interface {
+	PostArticleService(article models.Article) (models.Article, error)
+	GetArticleListService(page int) ([]models.Article, error)
+	GetArticleService(articleID int) (models.Article, error)
+	PostNiceService(article models.Article) (models.Article, error)
+
+	PostCommentService(comment models.Comment) (models.Comment, error)
+}
