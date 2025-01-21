86a7743702d539a5f5e8eb051d951406caac4bff
Tomoyuki Shimizu <smz2develop@gmail.com>
2025-01-21 19:11:24 +0900

    Chpater 7-2 done

--- services/comments_service.go
+++ services/comments_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/simics-ja/go-api-sample/apperrors"
 	"github.com/simics-ja/go-api-sample/models"
 	"github.com/simics-ja/go-api-sample/repositories"
 )
@@ -10,6 +11,7 @@ import (
 func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
 		return models.Comment{}, err
 	}
 
